6c6938ad7ce157f6580ee95db859cce541f434c2
Efrain May <efrain_may_75@hotmail.com>
2021-04-09 01:54:36 -0500

    rutas hijas mas lazy load de heroes

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -6,6 +6,8 @@ import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
+//modulos
+import { HeroesRoutingModule } from './heroes-routing.module';
 
 
 
@@ -15,10 +17,11 @@ import { ListadoComponent } from './pages/listado/listado.component';
     BuscarComponent,
     HeroeComponent,
     HomeComponent,
-    ListadoComponent
+    ListadoComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HeroesRoutingModule
   ]
 })
 export class HeroesModule { }
