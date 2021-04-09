35f2698c40aba31bebc38fe12546adf4b7317432
Efrain May <efrain_may_75@hotmail.com>
2021-04-09 01:33:43 -0500

    lazy load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 const routes: Routes = [
   {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
     path: '404',
     component: ErrorPageComponent
   },
