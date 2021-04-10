43084fc265146dd30cfc90b87fb35aa5076231e0
Efrain May <efrain_may_75@hotmail.com>
2021-04-10 13:14:49 -0500

    service de heroes mas obtencion de datsa por medio de jhttp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from "@angular/common/http";
 
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
@@ -16,7 +17,8 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
