42ccf3e597b4139eb9bd9aa4514c7eea98154fdf
Efrain May <efrain_may_75@hotmail.com>
2021-04-18 16:24:44 -0500

    enviroment optimizations

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -3,19 +3,22 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { Heroe } from '../interfaces/heroes.interface';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
 
+  private baseUrl: string = environment.baseUrl;
+
   constructor(private http: HttpClient) { }
 
   getHeroes(): Observable<Heroe[]>{
-    return this.http.get<Heroe[]>('http://localhost:3000/heroes');
+    return this.http.get<Heroe[]>(`${this.baseUrl}/heroes`);
   }
 
   getHeroePorId(id: string): Observable<Heroe>{
-    return this.http.get<Heroe>(`http://localhost:3000/heroes/${id}`);
+    return this.http.get<Heroe>(`${this.baseUrl}/heroes/${id}`);
   }
 }
