e773c29bedb5ad807618c89005d43cf1436d3eae
Efrain May <efrain_may_75@hotmail.com>
2021-04-23 02:18:07 -0500

    diseño pantalla login

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -11,6 +11,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from '@angular/material/select';
 
 
 @NgModule({
@@ -25,7 +26,8 @@ import { MatInputModule } from "@angular/material/input";
     MatProgressSpinnerModule,
     MatAutocompleteModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatSelectModule
   ]
 })
 export class MaterialModule { }
