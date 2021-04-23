1616471c82c2b0e18f56b5f76db020c929e4fd74
Efrain May <efrain_may_75@hotmail.com>
2021-04-23 02:34:42 -0500

    insertar en db + service endpoint

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -25,4 +25,8 @@ export class HeroesService {
   getSujerencias(termino: string): Observable<Heroe[]> {
     return this.http.get<Heroe[]>(`${this.baseUrl}/heroes?q=${ termino }&_limit=6`);
   }
+
+  agregarHeroe(heroe: Heroe): Observable<Heroe> {
+    return this.http.post<Heroe>(`${this.baseUrl}/heroes`, heroe);
+  }
 }
