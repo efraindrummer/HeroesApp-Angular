94e6fe5b308147b0bc0665bd7889ec38fc2e42ab
Efrain May <efrain_may_75@hotmail.com>
2021-04-30 15:10:38 -0500

    login basico

--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -11,11 +12,13 @@ import { Component, OnInit } from '@angular/core';
     `
   ]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
+  logout(){
+    //ir al backend
+    //un usuario
+    this.router.navigate(['./auth']);
   }
-
 }
